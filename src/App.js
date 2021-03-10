fd0bf4770f18c0aab926cbc44af8167e4627fd59
Ricardo Schroeder <ricardoschroeder@gmail.com>
2021-03-10 21:52:23 +0100

    fetching message from api (azure function)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [appState, setAppState] = useState({
+    loading: false,
+    value: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `http://localhost:7071/api/message`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((value) => {
+        console.log(value);
+        setAppState({ loading: false, value: value.text });
+      });
+  }, [setAppState]);
+  return (
+    <div>{appState.value}</div>
+  );
 }
 
 export default App;
